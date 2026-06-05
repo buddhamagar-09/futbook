d9abba9ce7c6ba3d6c1f49398142349ecddff9be
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-06-05 18:31:59 +0545

    routing given for admin dashboard with middleware

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,5 +16,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+//Admin Middleware
+Route::middleware(['admin'])->group(function () {
+      Route::get('/admin/dashboard', [AdminController::class, 'adminDashboard'])->name('admin.dashboard');
+      Route::get('/admin/users', [AdminController::class, 'Users'])->name('admin.users');
+});
 require __DIR__.'/auth.php';
