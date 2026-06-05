be985630b757f9a742d2d50ebe1b588bb80bf3c4
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-06-05 18:30:28 +0545

    admin name give for admin middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // $middleware->add(App\Http\Middleware\AdminMiddleware::class); it is used in route group
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
