03917545c1aabf09b75203aaabe50960a2316efa
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-06-08 22:32:57 +0545

    dashboard sections separated in each file

---
+++ resources/views/admin/home/sidebar.blade.php
@@ -0,0 +1,31 @@
+     <div class="d-flex align-items-stretch">
+    <!-- Sidebar Navigation-->
+    <nav id="sidebar">
+      <!-- Sidebar Header-->
+      <div class="sidebar-header d-flex align-items-center">
+        <div class="avatar"><img src="../../../../public/admin/img/buddha.jpg" alt="..." class="img-fluid rounded-circle"></div>
+        <div class="title">
+          <h1 class="h5">Buddha Saru Magar</h1>
+          <p>Web Designer</p>
+        </div>
+      </div>
+      <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
+      <ul class="list-unstyled">
+        <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
+        <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i
+              class="icon-windows"></i>Products</a>
+          <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
+            <li><a href="{{ route('add.products') }}">Add Products</a></li>
+            <li><a href="#">Page</a></li>
+            <li><a href="#">Page</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <!-- Sidebar Navigation end-->
+  
+    
+
+    
+      
+   
