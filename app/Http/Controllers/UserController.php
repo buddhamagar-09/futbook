ea746a5328928b097c144d0c1aa2a581e864843c
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-06-11 23:37:16 +0545

    fn to view all products and view product details

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class UserController extends Controller
 {
@@ -13,5 +14,17 @@ class UserController extends Controller
         } else if (Auth::check() && Auth::user()->usertype == 'admin') {
             return view('admin.dashboard');
         }
+    }
+
+    public function products()
+    {
+        $product = Product::all();
+        return view('frontend.products', ['productlist' => $product]);
+    }
+
+    public function product_details(string $id)
+    {
+        $product = Product::find($id);
+        return view('frontend.product_details', ['product' => $product]);
     }
 }
