c2241fbd69ac0abc97d3e29654e2a9d99124116c
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-07-17 15:24:47 +0545

    main home page of admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,16 +1,17 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-@include('admin.home.css')
-<body>
-    @include('admin.home.header')
-    @include('admin.home.sidebar')
-    <div class="page-content">
-        <div class="page-header">
-            <div class="container-fluid">
-                <h2 class="h5 no-margin-bottom">Dashboard</h2>
-            </div>
+@extends('admin.layouts.app')
+
+@section('content')
+
+<div class="container-fluid">
+
+    <h2 class="my-4">Dashboard</h2>
+
+    <div class="card">
+        <div class="card-body">
+            Welcome to MotoMods Nepal Admin Dashboard.
         </div>
-        @include('admin.home.footer')
-        @include('admin.home.js')
-</body>
-</html>
+    </div>
+
+</div>
+
+@endsection
