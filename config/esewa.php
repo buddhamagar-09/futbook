958947ec5f30703b80f38d6d3dbef57fbd34ddc7
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-09-10 22:52:02 +0545

    Config file for esewa required fields

---
+++ config/esewa.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'base_url' => env('ESEWA_BASE_URL'),
+
+    'payment_url' => env('ESEWA_PAYMENT_URL'),
+
+    'product_code' => env('ESEWA_PRODUCT_CODE'),
+
+    'secret_key' => env('ESEWA_SECRET_KEY'),
+
+    'status_url' => env('ESEWA_STATUS_URL'),
+
+    'success_url' => env('ESEWA_SUCCESS_URL'),
+
+    'failure_url' => env('ESEWA_FAILURE_URL'),
+
+];
