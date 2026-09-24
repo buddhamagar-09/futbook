c2e8f703bed9b6e9c70fe0e816f00690df1d254c
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-09-24 20:59:34 +0545

    project name changed

--- resources/views/frontend/tailwindcss.blade.php
+++ resources/views/frontend/tailwindcss.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+        <title>{{ config('app.name', 'FutBook') }}</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     </head>
